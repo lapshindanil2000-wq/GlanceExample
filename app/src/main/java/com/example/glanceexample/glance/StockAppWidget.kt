45ad725a14468a0c17909bc34c056e32256a20ca
ComposeDemo <danil.lapshin.2000@mail.ru>
2026-04-18 00:31:07 +0700

    update

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -6,6 +6,7 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.updateAll
 import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
@@ -13,13 +14,36 @@ import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.time.Duration
 
 class StockAppWidget : GlanceAppWidget() {
+    private var job: Job? = null
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        if (job == null) {
+            job = startUpdateJob(
+                Duration.ofSeconds(20).toMillis(),
+                context
+            )
+        }
         provideContent {
             GlanceTheme {
                 GlanceContent()
+            }
+        }
+    }
+
+    private fun startUpdateJob(timeInterval: Long, context: Context): Job {
+        return CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                PriceDataRepo.update()
+                updateAll(context)
+                delay(timeInterval)
             }
         }
     }
